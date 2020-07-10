dbb84aadd87ba957cf66ee397f401bec27f5b0d2
Pozo <huynhphuchau2040@gmail.com>
2020-07-10 11:22:20 +0700

    Update menu

--- ThuVien - Hoagluan58/Header/IO.hpp
+++ ThuVien - Hoagluan58/Header/IO.hpp
@@ -3,11 +3,173 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <windows.h>
+#include <conio.h>
 #include "json.hpp"
 #include "book.hpp"
 #include "Utils.hpp"
 
 using json = nlohmann::json;
+
+void ShowConsoleCursor(bool showFlag) {
+        HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_CURSOR_INFO cursorInfo;
+        GetConsoleCursorInfo(out, &cursorInfo);
+        cursorInfo.bVisible = showFlag; // set the cursor visibility
+        SetConsoleCursorInfo(out, &cursorInfo);
+}
+
+void gotoxy(int x, int y) {
+        SHORT sx = SHORT(x), sy = SHORT(y);
+        static HANDLE h = NULL;
+        if(!h) h = GetStdHandle(STD_OUTPUT_HANDLE);
+        COORD c = { sx, sy };
+        SetConsoleCursorPosition(h,c);
+}
+
+int getEdge(int num, int charnum){
+        return ((num-charnum)/2);
+}
+
+void getWindowsSize(int &rows, int &columns){
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+        GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+        columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+        rows = csbi.srWindow.Bottom - csbi.srWindow.Top +1;
+}
+
+void highLightChoose(std::string info, int y, int columns){
+        int x = getEdge(columns, info.length());
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 124);
+        gotoxy(x, y);
+        std::cout << info;
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+}
+
+void deHighLightChoose(std::string info, int y, int columns){
+        int x = getEdge(columns, info.length());
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+        gotoxy(x, y);
+        std::cout << info;
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+}
+
+void printMenu(std::string Menu[], int n, int rows, int columns){
+        ShowConsoleCursor(false);
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+        int first = getEdge(rows, n);
+        for(int i=0; i<n; i++) {
+                int temp = Menu[i].length();
+                temp = getEdge(columns, temp);
+                gotoxy(temp, first+i*2);
+                std::cout << Menu[i];
+        }
+}
+
+void Menu_Box (int rows, int columns,int speed=0, int showinfo=1){
+        system("CLS");
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+        ShowConsoleCursor(false);
+        std::cout << " " << std::setfill('_') << std::setw(columns-1) << " \n";
+        // Sleep(speed);
+        for (int i=1; i<rows-2; i++) {
+                std::cout << "||" << std::setfill(' ') << std::setw(columns-2) << "||\n";
+                // Sleep(speed);
+        }
+        std::cout << "||" << std::setfill('_') << std::setw(columns-3) << "||";
+        if (showinfo == 1) {
+                gotoxy(getEdge(columns, 39), 2);
+                std::cout << "Hoc Vien Cong Nghe Buu Chinh Vien Thong";
+                gotoxy(getEdge(columns, 35), 3);
+                std::cout << "De Tai Cuoi Ky Mon Cau Truc Du Lieu";
+                gotoxy(getEdge(columns, 21), 4);
+                std::cout << "GV: Luu Nguyen Ky Thu";
+                gotoxy(getEdge(columns, 28), 7);
+                std::cout << "CHUONG TRINH THI TRAC NGHIEM";
+                gotoxy(0, 8);
+                std::cout << "||" << std::setfill('_') << std::setw(columns-3) << "||";
+        }
+}
+
+void Menu_Admin (){
+        int pointer=0, prepointer=0, rows, columns;;
+        std::string Menu[5] = {"Quan li doc gia   ",
+                               "Quan li dau sach  ",
+                               "Them dau sach     ",
+                               "Muon tra sach     ",
+                               "Thoat             "};
+
+        getWindowsSize(rows, columns);
+        int firstMenuPosition = getEdge(rows, 5);
+
+        Menu_Box(rows, columns);
+        printMenu(Menu, 5, rows, columns);
+
+        //Read data here
+
+        while (true) {
+                int oldChoosePos = firstMenuPosition+prepointer*2;
+                int newChoosePos = firstMenuPosition+pointer*2;
+                deHighLightChoose(Menu[prepointer], oldChoosePos, columns);
+                highLightChoose(Menu[pointer], newChoosePos, columns);
+
+                while (true) {
+                        if (GetAsyncKeyState(VK_RETURN) & 0x8000) {
+                                if (pointer == 0) {
+                                        //Load data
+                                        Menu_Box(rows, columns);
+                                        printMenu(Menu, 5, rows, columns);
+                                        highLightChoose(Menu[pointer], newChoosePos, columns);
+                                        break;
+                                }
+                                else if (pointer == 1) {
+                                        //Load data
+                                        Menu_Box(rows, columns);
+                                        printMenu(Menu, 5, rows, columns);
+                                        highLightChoose(Menu[pointer], newChoosePos, columns);
+                                        //load data
+                                        break;
+                                }
+                                else if (pointer == 2) {
+
+
+                                        Menu_Box(rows, columns);
+                                        printMenu(Menu, 5, rows, columns);
+                                        highLightChoose(Menu[pointer], newChoosePos, columns);
+                                        break;
+                                }
+                                else if (pointer == 3) {
+
+                                        Menu_Box(rows, columns);
+                                        printMenu(Menu, 5, rows, columns);
+                                        highLightChoose(Menu[pointer], newChoosePos, columns);
+                                        break;
+                                }
+                                else if (pointer == 4) {
+                                        return;
+                                }
+                        }
+                        else if (GetAsyncKeyState(VK_UP) & 0x8000) {
+                                if (pointer > 0) {
+                                        prepointer = pointer;
+                                        pointer--;
+                                        break;
+                                }
+                        }
+                        else if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
+                                if (pointer < 4) {
+                                        prepointer = pointer;
+                                        pointer++;
+                                        break;
+                                }
+                        }
+                        else if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) {
+                                return;
+                        }
+                }
+                Sleep(125);
+        }
+}
 
 
 void BookToJson (std::string filename, ListOfBookPtr list){
--- ThuVien - Hoagluan58/Header/TestCode.cpp
+++ ThuVien - Hoagluan58/Header/TestCode.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
-#include "Borrow.hpp"
+#include "IO.hpp"
 
 int main(){
 
-        Borrow::DoubledLinkedList brr;
-        Borrow::InitDoubledLinkedList(brr);
-        Borrow::abook newbrr = {"12213", {1,4,2013}, {7, 5, 2013}, 0};
-        Borrow::insertNode(brr, newbrr);
+        Menu_Admin();
 
 
 
