1336e47f674caf572996e97eca114de62034f970
Pozo <huynhphuchau2040@gmail.com>
2020-07-11 19:55:37 +0700

    Update test code

--- ThuVien - Hoagluan58/Header/TestCode.cpp
+++ ThuVien - Hoagluan58/Header/TestCode.cpp
@@ -3,9 +3,16 @@
 #include <iomanip>
 #include "IO.hpp"
 
+
+
 int main(){
 
-        Menu_Admin();
+
+        Borrow::DoubledLinkedList root;
+        Borrow::InitDoubledLinkedList(root);
+        Borrow::abook mybook =  {"kkk", {1,3,2020}, {1,4,2020}, 0};
+        Borrow::insertNode(root, mybook);
+        std::cout << root.head->data << '\n';
 
 
 
