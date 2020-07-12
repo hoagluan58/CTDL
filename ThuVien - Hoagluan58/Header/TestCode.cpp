8f49226fb399295b3fd8064602360644c4a982fd
Pozo <huynhphuchau2040@gmail.com>
2020-07-12 20:37:09 +0700

    update menu, 60%

--- ThuVien - Hoagluan58/Header/TestCode.cpp
+++ ThuVien - Hoagluan58/Header/TestCode.cpp
@@ -1,18 +1,9 @@
-#include <iostream>
-#include <fstream>
-#include <iomanip>
 #include "IO.hpp"
 
 
 
 int main(){
-
-        Borrow::DoubledLinkedList root;
-        Borrow::InitDoubledLinkedList(root);
-        Borrow::abook mybook =  {"kkk", {1,3,2020}, {1,4,2020}, 0};
-        Borrow::insertNode(root, mybook);
-        std::cout << root.head->data << '\n';
-
+        Menu_Admin();
 
 
 
